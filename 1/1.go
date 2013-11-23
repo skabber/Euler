bfc9d1228def1e4c1d69ce2b0e35cd177ffddf20
Jay Graves <jay@doubleencore.com>
2013-11-22 17:00:20 -0700

    Just a slight refactor.

--- 1/1.go
+++ 1/1.go
@@ -4,10 +4,13 @@ import "fmt"
 
 func main() {
     i := 0
-    for x := 1; x < 1001; x++ {
-        if (x % 3 == 0) || (x % 5 == 0) {
-            i = i + x
+    sum := 0
+    for i < 1001 {
+        i++
+        if (i % 3 == 0) || (i % 5 == 0) {
+            sum = sum + i
         }
+
     }
-    fmt.Println(i)
+    fmt.Println(sum)
 }
